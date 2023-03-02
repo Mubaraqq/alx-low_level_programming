42bbc45a22e6e4da7135ddda7fc377f88081de14
Mubaraqq <mubaraqabba@gmail.com>
2023-03-02 14:38:37 -0800

    fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,10 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
+	last = n % 10;
 
 	if (n < 0)
-		n *= -1;
-	_putchar('0' + n);
-	return (n);
+		last *= -1;
+	_putchar(last + '0');
+	return (last);
 }
